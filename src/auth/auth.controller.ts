cd088db0d15c935bb35b0feb5673781f220dbffe
T0192578 <1zDAHL7XwE2ptKyPGDxX>
2019-03-19 11:57:28 +0100

    (WIP) Discord oauth integration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,53 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {BadRequestException, Controller, Get, Query, Req, Res} from '@nestjs/common';
+import {Request, Response} from 'express';
 import {AuthService} from './auth.service';
-import {CredentialsDto} from './dto/credentials.dto';
-import {CreateUserDto} from '../user/dto/create-user.dto';
+import fetch from 'node-fetch';
 
 @Controller('auth')
 export class AuthController {
+
+  private readonly CLIENT_ID = process.env.CLIENT_ID;
+  private readonly CLIENT_SECRET = process.env.CLIENT_SECRET;
+  private readonly redirectEndpoint = 'api/auth/callback';
+  private readonly scopes = ['identify'];
+
   constructor(private readonly authService: AuthService) {
   }
 
-  @Post('login')
-  public async login(@Body() credentials: CredentialsDto): Promise<any> {
-    return await this.authService.login(credentials.email, credentials.password);
+  @Get('login')
+  public async login(@Res() res: Response, @Req() req: Request): Promise<any> {
+
+    const redirect = this.getRedirectURI(req);
+
+    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${this.CLIENT_ID}&scope=${this.scopes.join()}` +
+      `&response_type=code&redirect_uri=${redirect}`);
+    // return await this.authService.login(credentials.email, credentials.password);
   }
 
-  @Post('register')
-  public async register(@Body()userDto: CreateUserDto) {
-    return await this.authService.register(userDto);
+  @Get('callback')
+  public async callback(@Req() req: Request, @Query('code')code: string) {
+    if (!code) {
+      throw new BadRequestException();
+    }
+    const redirect = this.getRedirectURI(req);
+    const creds = Buffer.from(`${this.CLIENT_ID}:${this.CLIENT_SECRET}`).toString('base64');
+    const response = await fetch(`https://discordapp.com/api/oauth2/token?code=${code}&redirect_uri=${redirect}&scope=${this.scopes.join()}` +
+      `&grant_type=authorization_code`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${creds}`,
+        },
+      });
+    return await response.json();
   }
+
+  private getRedirectURI(req: Request) {
+    return req.protocol + '://' + req.get('Host') + '/' + this.redirectEndpoint;
+  }
+
+  // @Post('register')
+  // public async register(@Body()userDto: CreateUserDto) {
+  //   return await this.authService.register(userDto);
+  // }
 }
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secretKey',
+      secretOrKey: process.env.CLIENT_SECRET,
     });
   }
 
