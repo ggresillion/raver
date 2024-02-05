7a47b3d718391fd0decbaa40870c7da1708ab1cd
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2024-02-05 12:59:46 +0100

    fixed all issues with skipping

--- test/integration_test.go
+++ test/integration_test.go
@@ -4,33 +4,33 @@ import (
 	"raver/audio"
 	"raver/youtube"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 const (
 	guildID = "test"
-	videoID = "hqS4u4aiZ8g"
+	videoID = "VUfvRciny_Y"
 )
 
 func TestPlayerPlaySingleTrack(t *testing.T) {
 	player := audio.NewPlayer(guildID)
-	change := player.OnChange()
 	track, err := youtube.GetPlayableTrackFromYoutube(videoID)
 	if err != nil {
 		t.Errorf("error loading from youtube: %v", err)
 	}
 	player.Add(track)
 	<-player.LineOut
-	assert.NotNil(t, <-change)
+	assert.NotNil(t, <-player.Change)
 }
 
 func TestPlayerSkip(t *testing.T) {
 	player := audio.NewPlayer(guildID)
-	change := player.OnChange()
 	go func() {
 		for {
 			<-player.LineOut
+			time.Sleep(10 * time.Millisecond)
 		}
 	}()
 
@@ -39,17 +39,72 @@ func TestPlayerSkip(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, audio.IDLE, player.State)
 	player.Add(track1)
-	<-change
+	<-player.Change
 	assert.Equal(t, audio.Playing, player.State)
-	<-change
+	assert.Equal(t, 1, len(player.Queue))
 
 	track2, err := youtube.GetPlayableTrackFromYoutube(videoID)
 	assert.Nil(t, err)
 	player.Add(track2)
+	<-player.Change
+	assert.Equal(t, audio.Playing, player.State)
 	assert.Equal(t, 2, len(player.Queue))
 	player.Skip()
-	<-change
-	<-change
+	<-player.Change
 	assert.Equal(t, audio.Playing, player.State)
 	assert.Equal(t, 1, len(player.Queue))
+	player.Skip()
+	assert.Equal(t, audio.Playing, player.State)
+	assert.Equal(t, 1, len(player.Queue))
+}
+
+func TestPlayerAutostop(t *testing.T) {
+	player := audio.NewPlayer(guildID)
+	go func() {
+		for {
+			<-player.LineOut
+			time.Sleep(10 * time.Millisecond)
+		}
+	}()
+
+	track, err := youtube.GetPlayableTrackFromYoutube(videoID)
+	if err != nil {
+		t.Errorf("error loading from youtube: %v", err)
+	}
+	player.Add(track)
+	<-player.Change
+	assert.Equal(t, audio.Playing, player.State)
+	assert.Equal(t, 1, len(player.Queue))
+	<-player.Change
+	assert.Equal(t, audio.IDLE, player.State)
+	assert.Equal(t, 0, len(player.Queue))
+}
+
+func TestPlayerAutoskip(t *testing.T) {
+	player := audio.NewPlayer(guildID)
+	go func() {
+		for {
+			<-player.LineOut
+			time.Sleep(10 * time.Millisecond)
+		}
+	}()
+
+	// get some tracks
+	track1, err := youtube.GetPlayableTrackFromYoutube(videoID)
+	assert.Nil(t, err)
+	track2, err := youtube.GetPlayableTrackFromYoutube(videoID)
+	assert.Nil(t, err)
+	assert.Equal(t, audio.IDLE, player.State)
+	player.Add(track1)
+	player.Add(track2)
+	<-player.Change
+	<-player.Change
+	assert.Equal(t, audio.Playing, player.State)
+	assert.Equal(t, 2, len(player.Queue))
+	<-player.Change
+	assert.Equal(t, audio.Playing, player.State)
+	assert.Equal(t, 1, len(player.Queue))
+	<-player.Change
+	assert.Equal(t, audio.IDLE, player.State)
+	assert.Equal(t, 0, len(player.Queue))
 }
