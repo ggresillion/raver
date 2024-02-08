5cfa0afa21ae512021c392b1d03ed21a9052dedc
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2024-02-08 16:02:12 +0100

    progress bar

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"log"
 	"os"
 	"os/signal"
 	"raver/discord"
+	"raver/youtube"
 	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 var token string
@@ -19,9 +23,17 @@ func Start(bot *discord.Bot) {
 		panic(err)
 	}
 	defer bot.Stop()
+
+	gb, _ := bot.Guild("423590632582414346")
+	gb.JoinUserChannel("169143950203027456")
+	t, _ := youtube.GetPlayableTrackFromYoutube("2ZouETXmFnM")
+	gb.Player.Add(t)
+	gb.PrintPlaylist(bot.Session(), &discordgo.Interaction{ChannelID: "1205166368056680498"})
+
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	<-sigs
+	log.Println("exiting...")
 }
 
 func main() {
