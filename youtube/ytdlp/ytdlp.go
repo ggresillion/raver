f99dfe813eafa62f4624a4bc06a1feb6621f8b72
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2025-02-19 21:08:08 +0100

    Revert "running yt-dlp as a separate process"
    This reverts commit 8e599619dd914024d179483e980db3b3dd48f8d4.

--- youtube/ytdlp/ytdlp.go
+++ youtube/ytdlp/ytdlp.go
@@ -7,16 +7,9 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"sync"
 )
 
-var (
-	path       string
-	cmd        *exec.Cmd
-	stdinPipe  io.WriteCloser
-	stdoutPipe io.ReadCloser
-	mu         sync.Mutex
-)
+var path string
 
 const (
 	url    = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp"
@@ -26,60 +19,31 @@ const (
 func init() {
 	if _, err := exec.LookPath(binary); err != nil {
 		// If not, download it
-		var err error
 		path, err = downloadYtdlp()
 		if err != nil {
 			panic(err)
 		}
-	} else {
-		slog.Info("ytdlp: yt-dlp found")
-		path = binary
+		return
 	}
-
-	// Start the persistent yt-dlp process
-	startPersistentProcess()
-}
-
-func startPersistentProcess() {
-	mu.Lock()
-	defer mu.Unlock()
-
-	cmd = exec.Command(path, "--no-cache-dir", "--no-progress", "-J", "-")
-	var err error
-	stdinPipe, err = cmd.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
-	stdoutPipe, err = cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-
-	if err := cmd.Start(); err != nil {
-		panic(err)
-	}
-
-	slog.Info("ytdlp: started persistent yt-dlp process")
+	slog.Info("ytdlp: yt-dlp found")
+	path = binary
 }
 
 func GetVideoByID(ID string) (*Video, error) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	// Write the video ID to the stdin of the persistent process
-	_, err := io.WriteString(stdinPipe, ID+"\n")
+	// Use the installed yt-dlp binary
+	cmd := exec.Command(path, "-f", "bestaudio", "-J", ID)
+	output, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
 
-	// Read the JSON output from stdout
-	decoder := json.NewDecoder(stdoutPipe)
-	var video Video
-	if err := decoder.Decode(&video); err != nil {
+	var video *Video
+	err = json.Unmarshal(output, &video)
+	if err != nil {
 		return nil, err
 	}
 
-	return &video, nil
+	return video, nil
 }
 
 func downloadYtdlp() (string, error) {
@@ -107,6 +71,6 @@ func downloadYtdlp() (string, error) {
 	}
 	tmpFile.Close()
 
-	slog.Info("ytdlp: yt-dlp downloaded at " + tmpFile.Name())
+	slog.Info("ytdlp: yd-dlp downloaded at " + tmpFile.Name())
 	return tmpFile.Name(), nil
 }
